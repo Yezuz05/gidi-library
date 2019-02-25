8d877f743ece516b88769f7b48608fd3c8013b20
yezuz05 <temitopeyussuph@gmail.com>
2019-02-25 02:33:15 +0100

    feat: created books-list and add-book routes and components

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 const dashboardRoutes: Routes = [
-  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'dashboard',  component: DashboardComponent,
+     children: [
+        {path: '', redirectTo: 'books', pathMatch: 'full'},
+        {path: 'books', component: BooksListComponent},
+        {path: 'books/add-book', component: AddBookComponent},
+    ]
+   },
 ];
  
 @NgModule({
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MaterialModule } from '../material.module';
+import { AddBookComponent } from './add-book/add-book.component';
+import { BooksListComponent } from './books-list/books-list.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, AddBookComponent, BooksListComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
