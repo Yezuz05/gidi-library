7cefbbd235875fe578b162127c8e5a3b9c44ffa3
yezuz05 <temitopeyussuph@gmail.com>
2019-02-25 04:47:37 +0100

    feat: readers list ui complete

--- src/app/dashboard/readers-list/readers-list.component.ts
+++ src/app/dashboard/readers-list/readers-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-readers-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadersListComponent implements OnInit {
 
-  constructor() { }
+  searchControl= this.fb.control(null);
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
