8fa5bcf4d7aac80d119559df8c8d76bcd8ebb2a2
yezuz05 <temitopeyussuph@gmail.com>
2019-03-06 22:37:47 +0100

    update: environment prod file update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCZyfUNC9xajrBs7pVKTeHsJzqBCB__pFQ",
+    authDomain: "gidi-library.firebaseapp.com",
+    databaseURL: "https://gidi-library.firebaseio.com",
+    projectId: "gidi-library",
+    storageBucket: "gidi-library.appspot.com",
+    messagingSenderId: "71525992901"
+  }
 };
